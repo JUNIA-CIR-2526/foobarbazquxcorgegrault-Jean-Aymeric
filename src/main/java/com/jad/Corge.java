dd3214ff7c2272bcc4335c3932c9cd8e2fa9e714
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2025-09-22 08:56:02 +0200

    done

---
+++ src/main/java/com/jad/Corge.java
@@ -0,0 +1,20 @@
+package com.jad;
+
+public class Corge {
+    private Foo foo;
+
+    public Corge(final Foo foo) {
+        this.setFoo(foo);
+    }
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+    public void setFoo(final Foo foo) {
+        if (this.foo != null) this.foo.setCorge(null);
+        this.foo = foo;
+        if (foo.getCorge() != this) foo.setCorge(this);
+    }
+
+}
---
+++ src/main/java/com/jad/Grault.java
@@ -0,0 +1,15 @@
+package com.jad;
+
+public class Grault {
+    private Foo foo;
+
+    public Grault(final Foo foo) {
+        this.foo = foo;
+    }
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+
+}
